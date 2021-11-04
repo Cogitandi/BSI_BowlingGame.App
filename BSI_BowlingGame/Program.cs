5ad78f83ca309ce2b14592d04ca880644f2719b3
Pawel <p.kapica6@gmail.com>
2021-11-04 14:42:41 +0100

    upload empty project

---
+++ BSI_BowlingGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BSI_BowlingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
